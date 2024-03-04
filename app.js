78913e1fbad4f07aefe37e83eaf0a150d2f7dbe0
RS_ict <r.stefani87.informatica@gmail.com>
2024-03-04 12:29:11 +0100

    no message

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-import * as app from "../../../app.js"; 
+import {getApplicationRoot} from "@ares/core";
+import * as app from "../../../app.js";
+import { dirname } from "path"; // Import the path module
+
 /**
- * 
- * @param {string} url 
+ *
+ * @param {string} url
  * @returns {boolean|{type:string,domain:string}}
- * 
+ *
  * @desc {en} Check if url corresponds to production environment
  * @desc {it} Verifica se l'url corrisponde alla produzione
  * @desc {es} Comprueba si la url corresponde a la producción
@@ -13,10 +16,15 @@ import * as app from "../../../app.js";
  * @desc {ja} プロダクション環境と一致するか確認
  * @desc {zh} 检查 URL 是否与生产环境匹配
  * @desc {ru} Проверьте, соответствует ли URL продуктивной среде
- * 
+ *
  * */
 export function isProduction(url) {
-  return environments.some(x=>url.toLowerCase().startsWith(x.domain) && x.type === "production")[0]??false;
+  return (
+    environments.some(
+      (x) => url.toLowerCase().startsWith(x.domain) && x.type === "production"
+    )[0] ?? false
+  );
 }
- 
+
+app.root=getApplicationRoot();
 export default app;
